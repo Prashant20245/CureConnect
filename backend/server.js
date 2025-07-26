fed7d29c58a0d9c86972bf6463ba894c4dc696a1
Prashant20245 <prashantkumar20245@gmail.com>
2025-07-26 15:00:55 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,8 @@ connectCloudinary();
 
 //middlewares
 app.use(express.json());
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5174", "https://cure-connect-ten.vercel.app",  
+  "https://cureconnect-admin.vercel.app" ];
 
 app.use(
   cors({
